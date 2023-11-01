52834e9e4807f19630a1c007da498d5ed7ea9b8a
zhangyadong <1558628010@qq.com>
2023-11-01 22:57:42 +0800

    feat:synchronized 4种加锁方式

--- concurrency/src/main/java/com/java/concurrency/sync/S04_T.java
+++ concurrency/src/main/java/com/java/concurrency/sync/S04_T.java
@@ -16,7 +16,8 @@ public class S04_T {
 
     public static void main(String[] args) {
         /*
-            考虑下这里写synchronized (this) 是否可以？ 答案：不可以会报错
+            考虑下这里写synchronized (this) 是否可以？ 答案：不可以会报错,原因在于static属于类，先于任何对象存在，this指向的是对象，
+            也就是说this还无法指向对象引用，静态方法就已经存在了，在运行静态方法时，无法获取到当前对象引用，从而导致出现异常。所以在静态方法中无法使用this关键字
          */
         synchronized (S04_T.class) {
             count--;
